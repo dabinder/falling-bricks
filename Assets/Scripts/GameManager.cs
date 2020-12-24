4b17bfcc7452e924eff5ba5630a57abee6477010
Dan Binder <dbinder9@gmail.com>
2020-12-24 11:37:38 -0500

    handle pause and restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 namespace Bricks {
 	public class GameManager : MonoBehaviour {
 		[SerializeField] GameObject playfieldObject;
-		[SerializeField] GameObject pausePanel;
-		[SerializeField] GameObject loseText;
+		[SerializeField] GameObject suspendPanel, losePanel, pausePanel;
 
 		private Playfield _playfield;
+		private bool _gameOver;
 
-		private bool _isStopped;
-		private bool IsStopped {
-			get => _isStopped;
+		private bool _isPaused;
+		private bool IsPaused {
+			get => _isPaused;
 			set {
-				_isStopped = value;
-				pausePanel.SetActive(value);
+				_isPaused = value;
+				suspendPanel.SetActive(value);
 				Time.timeScale = value ? 0 : 1;
 			}
 		}
@@ -23,14 +25,37 @@ namespace Bricks {
 		/// </summary>
 		private void Start() {
 			_playfield = playfieldObject.GetComponent<Playfield>();
+			Time.timeScale = 1;
 		}
 
 		/// <summary>
 		/// end game with a game over notification
 		/// </summary>
 		private void GameOver() {
-			IsStopped = true;
-			loseText.SetActive(true);
+			IsPaused = true;
+			losePanel.SetActive(true);
+			_gameOver = true;
+		}
+
+		/// <summary>
+		/// handle pause button press
+		/// </summary>
+		/// <param name="_">pause button press</param>
+		private void OnPause(InputValue _) {
+			if (_gameOver) return;
+			bool paused = !IsPaused;
+			IsPaused = paused;
+			pausePanel.SetActive(paused);
+		}
+
+		/// <summary>
+		/// confirm reset after end of game or from pause menu
+		/// </summary>
+		/// <param name="_">confirm button press</param>
+		private void OnConfirm(InputValue _) {
+			if (IsPaused) {
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			}
 		}
 	}
 }
